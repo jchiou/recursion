be7a52204977b9be17372e4680eeebd5ad0cbeaf
Jessica Chiou <jessica.chiou@gmail.com>
2016-06-03 12:32:25 -0700

    Added getElementsByClassName, specs pass

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -3,8 +3,47 @@
 //   return document.getElementsByClassName(className);
 // };
 
+
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
-};
+var getElementsByClassName = function (className) {
+    // need array of all class names
+    var results = [];
+    var body = document.body;
+    
+    // recursive function for walking the dom
+    function walkDom (node) {
+        // if childNodes exist...
+        if (node !== undefined) {
+            // and if it is are an array of nodes, then iterate over each node and call on each node recursively
+            if (node.length) {
+                for (var i = 0 ; i < node.length; i++) {
+                    if (node[i].classList !== undefined) {
+                        walkDom(node[i])
+                    }
+                }
+            }
+            // individual nodes go here
+            // if the node has a classList that contains the className, push into array
+            // then call recursively on its childNodes
+            else {
+                if (node.classList) {
+                    if (node.classList.contains(className)) {
+                        results.push(node)
+                    }
+                    walkDom(node.childNodes);
+                }
+            }
+        }
+
+        
+        
+    }
+    walkDom(body);
+    console.log(results);
+    //termination case
+    return results;
+
+}
+
+ 
+getElementsByClassName('targetClassName');
